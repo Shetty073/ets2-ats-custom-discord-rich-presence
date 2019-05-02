4dd3e6e53b634bc29235165b862a9eda70869dac
Ashish Shetty aka AlphaSierra <30448930+Shetty073@users.noreply.github.com>
2019-05-02 16:30:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ets2-custom-discord-rich-presence
+
+## What is this?
+### A custom discord rich presence script written in python using Pypresence for Euro Truck Simulator 2 or American Truck Simulator which uses ETS 2/ATS local Telemetry server.
+
+### In order for this to work you will require ETS 2/ATS Telemetry server. Download it from its [GitHub page](https://github.com/Funbit/ets2-telemetry-server) for free.
+
+## Screenshots and how to use example will be added soon...
---
+++ ets2_ats_custom_rich_presence.py
@@ -0,0 +1,95 @@
+from pypresence import Presence
+import requests as req
+import json
+import time
+import logging
+
+print("\n"
+      "\n"
+      "    ##############################################################\n"
+      "    # ETS 2 Custom Discord Rich Presence script by Ashish Shetty #\n"
+      "    ##############################################################\n"
+      "\n"
+      "  please make sure that ETS2/ATS telemetry server is running\n"
+      "  before you opened this file..\n"
+      "\n"
+      "  https://github.com/Shetty073/ets2-custom-discord-rich-presence\n")
+
+
+def get_data():
+    try:
+        response = req.get("http://192.168.0.100:25555/api/ets2/telemetry")
+        raw_data = json.loads(response.content)
+        return raw_data
+    except Exception as e:
+        logging.basicConfig(filename='error.log', level=logging.INFO)
+        logging.error(f"\nFatal error: \n{str(e)}\n")
+        print("\nFatal error: Please make sure that the ETS 2 / ATS Telemetry server is open before "
+              "running this application.\n")
+        input("Press enter key to exit....")
+        exit(1)
+
+
+def get_details():
+    raw_data = get_data()
+    game_data = raw_data["game"]
+    truck_data = raw_data["truck"]
+
+    if not game_data["connected"]:
+        details = "Simulation not started"
+    else:
+        if game_data["paused"]:
+            details = "Simulation paused"
+        else:
+            if truck_data["engineOn"]:
+                engine = "On"
+            else:
+                engine = "Off"
+            details = f'Driving: {truck_data["make"]} {truck_data["model"]}, Engine: {engine}'
+    return details
+
+
+def get_state():
+    raw_data = get_data()
+    game_data = raw_data["game"]
+    truck_data = raw_data["truck"]
+
+    if not game_data["connected"]:
+        state = "Waiting for process to hook"
+    else:
+        if game_data["paused"]:
+            state = "Driver drinking coffee"
+        else:
+            speed = int(truck_data["speed"])
+            rpm = int(truck_data["engineRpm"])
+            if speed < 0:
+                speed = 0
+            if int(truck_data["displayedGear"]) <= 0:
+                if int(truck_data["displayedGear"]) < 0:
+                    gear = "R"
+                else:
+                    gear = "N"
+            else:
+                gear = truck_data["displayedGear"]
+            fuel = (int(truck_data["fuel"]) / int(truck_data["fuelCapacity"]) * 100)
+            state = f'{speed} Km/hr {rpm} RPM Gear: {gear} Fuel: {int(fuel)}% left'
+    return state
+
+
+try:
+    client_id = "client_id"
+    RPC = Presence(client_id)
+    RPC.connect()
+    print("Running...\nYou can close this window after you finish playing ETS 2 or ATS...")
+    while True:
+        RPC.update(state=get_state(), details=get_details(), large_image="Euro Truck Simulator 2",
+                   large_text="Large Text Here!",
+                   small_image="small-image", small_text="Small Text Here!")
+        time.sleep(0.1)
+except Exception as e:
+    logging.basicConfig(filename='error.log', level=logging.INFO)
+    logging.error(f"\nFatal error: \n{str(e)}\nPlease make sure that Discord is open and running...")
+    print("\nFatal error: Please make sure that Discord is open before "
+          "running this application.\n")
+    input("Press enter key to exit....")
+    exit(1)
